1049f5816cee729894aaedc60cb58d459be8211d
Binod Chaudhary <thebinod7@gmail.com>
2023-11-09 09:20:30 +0545

    add react app

---
+++ apps/admin/src/app/nx-welcome.tsx
@@ -0,0 +1,11 @@
+export function NxWelcome({ title }: { title: string }) {
+  return (
+    <>
+      <div className="wrapper">
+        <h3>Welcome to NX</h3>
+      </div>
+    </>
+  );
+}
+
+export default NxWelcome;
